381954fa6eaf5917d13e3dde38a2bc820bdf5fa8
Mirajus Salehin <mirajussalehin@gmail.com>
2021-06-05 19:26:53 +0600

    preview deployment rebuild

---
+++ dist/_nuxt/static/1622899277/ourteam/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622899277",layout:"default",error:a,serverRendered:true,routePath:"\u002Fourteam",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e3bca205"}}}}(null));
