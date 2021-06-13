0e04c5f25a6d7dec0bb61ff6fb0f1931d3e3e982
Mirajus Salehin <mirajussalehin@gmail.com>
2021-06-13 22:13:40 +0600

    some text paraphrased

--- pages/index.vue
+++ pages/index.vue
@@ -16,21 +16,27 @@
             leading-loose
           "
         >
-          An online radio service in Bangladesh Agrciltural University that is
-          run by the students, for the students, to the students. Tune in more
-          talks like our campus life. We also arrange expert talks on
-          agriculture for farmers.
+          BAU Radio is an online radio station of Bangladesh Agricultural
+          University, administrated by its student.The very first online
+          streaming platform has come to light in the <b>60-years history</b> of
+          BAU. The dream has come true with a group of dedicated individuals.
+          The Passion did make history. It will provide essential daily news
+          from BAU's various locations. On the other hand, relieves monotony and
+          gives peace to BAU residents with its academic and amusing
+          programming.
         </p>
         <div class="ml-10 mt-4 mb-4">
           <button class="btn">Tune in</button>
         </div>
       </div>
-      <div class="w-full md:w-3/5"><svg1 class="mx-auto"/></div>
+      <div class="w-full md:w-3/5"><svg1 class="mx-auto" /></div>
     </section>
     <!--who are we-->
     <section class="md:flex md:flex-col mt-24 mb-10">
       <div class="mx-auto mb-10">
-        <h1 class="mx-auto text-center font-bold text-3xl lg:text-5xl">Who are we</h1>
+        <h1 class="mx-auto text-center font-bold text-3xl lg:text-5xl">
+          Who are we
+        </h1>
       </div>
       <div
         class="
@@ -56,10 +62,11 @@
       <div class="w-full md:w-3/5"><svg2 /></div>
       <div class="w-full md:w-2/5 m-auto text-center">
         <h1 class="title-text">Echoes of dreams</h1>
-        <p class="pt-10 pr-4 ml-10 mr-10">
+        <p class="pt-10 pr-4 ml-10 mr-10 leading-loose">
           Evreyone is a dreamer. But not everyone has the right platform to
           share their idea. We will give you that platform. BAU Radio is your
-          own platform to share your dreams.
+          own platform to share your dreams. Our goal is to take "Echoes of
+          Dreams" from BAU to every corner of the country.
         </p>
       </div>
     </section>
@@ -67,7 +74,7 @@
     <section class="md:flex space-x-1 mt-10">
       <div class="w-full md:w-2/5 m-auto">
         <h1 class="title-text text-center">Got anything to share?</h1>
-        <p class="pt-10 px-4 mx-10 text-justify">
+        <p class="pt-10 px-4 mx-10 text-justify leading-loose">
           We will help you to share to the whole university. Through our
           platform you will have access to the thousands of students of our
           university and others.
@@ -90,7 +97,8 @@ export default {
   components: { svg1, svg2, svg3, whocard },
   head() {
     return {
-      title: "BAU Radio",
+      title:
+        "BAU Radio - An online radio service Of Bangladesh Agricultural University",
     };
   },
   data() {
@@ -135,7 +143,7 @@ export default {
   @apply hidden md:block py-2 mx-10 text-xl lg:text-2xl text-brand-black tracking-wider font-semibold;
 }
 .btn {
-  @apply bg-brand-green text-white hover:shadow-md hover:bg-brand-dark transform hover:translate-x-2 font-bold rounded-full px-8 py-2 transition-all ease-out duration-200;
+  @apply focus:ring-0 bg-brand-green text-white hover:shadow-md hover:bg-brand-dark transform hover:translate-x-2 font-bold rounded-full px-8 py-2 transition-all ease-out duration-200;
 }
 html {
   scroll-behavior: smooth;
