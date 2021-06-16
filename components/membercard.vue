95aa2a3481681410896ac8374476992feb95f875
Mirajus Salehin <mirajussalehin@gmail.com>
2021-06-16 20:47:37 +0600

    added routes to team page

--- components/membercard.vue
+++ components/membercard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-4 max-w-md h-full flex flex-col gap-5 place-items-center justify-items-center">
+  <div class="p-4 w-full h-full flex flex-col gap-5 place-items-center justify-items-center">
     <img :src="img" alt="" class="w-36 h-36 rounded-full mx-auto" />
     <div class="mx-auto">
         {{name}}
