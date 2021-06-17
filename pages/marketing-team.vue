b8137ebe4fecb6c19bec75f613ba206b412c1b4d
Mirajus Salehin <mirajussalehin@gmail.com>
2021-06-17 12:46:08 +0600

    marketing team page modified

--- pages/marketing-team.vue
+++ pages/marketing-team.vue
@@ -1,18 +1,33 @@
 <template>
-  <section class="mt-10 min-h-screen">
+  <section class="mt-10 mb-10 min-h-screen">
     <div class="flex flex-col place-items-center justify-items-center pb-10">
-      <h1 class="text-center text-xl lg:text-5xl font-bold my-10 tracking-widest">
+      <h1
+        class="text-center text-xl lg:text-5xl font-bold my-10 tracking-widest"
+      >
         Meet our {{ members.teamName }} team
       </h1>
-      <h3 class="text-center text-brand-green text-base lg:text-xl">
-        They are {{ members.subtitle.toLowerCase() }}
+      <h3 class="text-center text-xl my-10 tracking-widest text-brand-green">
+        {{ members.subtitle }}
       </h3>
+    </div>
+    <div class="flex place-items-stretch justify-items-center mb-10">
+      <div class="mx-auto">
+        <p class="tracking-wide font-bold text-brand-green text-center">
+          Head of {{ members.teamName }}
+        </p>
+        <membercard :img="members.head.img" :name="members.head.name" />
+      </div>
+    </div>
+    <div class="mx-auto max-w-7xl mb-10 mt-10">
+      <p class="tracking-wide font-bold text-brand-green text-center">
+        Genaral Members
+      </p>
     </div>
     <div
       class="
         mx-auto
         grid grid-cols-1
-        md:grid-cols-3
+        md:grid-cols-2
         gap-4
         place-items-center
         justify-items-center
@@ -36,9 +51,13 @@ export default {
     return {
       members: {
         teamName: "Marketing",
-        subtitle: "The geeks of technolgy",
+        subtitle: "The promoter of business",
         logo: "/team/logos/logo_it.svg",
         slug: "marketing-team",
+        head:{
+          name:"Sayra Islam Saki",
+          img:"https://i.imgur.com/g0pX4bm.png"
+        },
         members: [
           {
             name: "Khoshnur Yasmin Ratna",
