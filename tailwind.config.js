d235a439d198239b2402efadf766bdd90d2f128f
Mirajus Salehin <mirajussalehin@gmail.com>
2021-06-20 21:16:12 +0600

    colors extended

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,19 @@ module.exports = {
         "brand-green": '#297880',
         "brand-dark": '#263238',
         "brand-black": "#204E52",
-        "brand-green-lighter": "#86BAB6"
+        "brand-green-lighter": "#86BAB6",
+        "brand": {
+          "50": "#112106105",
+          "100": "#f3fbfb",
+          "200": "#b6e3e8",
+          "300": "#78cbd4",
+          "400": "#3db0bd",
+          "500": "#29777f",
+          "600": "#216168",
+          "700": "#1b4f55",
+          "800": "#143a3e",
+          "900": "#0c2427"
+        }
       }
     },
   },
