11ec443ef20a699b145ad22b15c0cfd64d5342fa
Mirajus Salehin <mirajussalehin@gmail.com>
2021-06-24 00:03:23 +0600

    adding google site verification

---
+++ dist/_nuxt/static/1624471290/graphics-designer-team/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624471290",layout:"default",error:a,serverRendered:true,routePath:"\u002Fgraphics-designer-team",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6ccc12b8"}}}}(null));
