34c9e7954a2a1b07007db609c78b0d527b22dfa2
Mirajus Salehin <mirajussalehin@gmail.com>
2021-06-24 00:35:10 +0600

    added sitemap.xml and robots.txt

--- dist/_nuxt/static/1624471290/finance-team/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624471290",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffinance-team",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6ccc12b8"}}}}(null));
