b120a59ed542fa2aa4e508ba27274efa17dd32ea
sayem <mirazussalehin@outlook.com>
2021-08-15 16:39:32 +0000

    downgarde @nuxt/iamge to 0.4.7

---
+++ dist/_nuxt/static/1629045456/video-editing-team/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629045456",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvideo-editing-team",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4ffc2ef4"}}}}(null));
