57fe4e71d1783b4710f05ba0bdda82f76fde5b80
sayem <mirazussalehin@outlook.com>
2021-08-16 08:47:59 +0000

    removed start script from package.json

---
+++ dist/_nuxt/static/1629103642/strategy-and-planning-team/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629103642",layout:"default",error:a,serverRendered:true,routePath:"\u002Fstrategy-and-planning-team",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ac0878ec"}}}}(null));
